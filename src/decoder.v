2d8cfad6ae4f26bf3f423e67c1aab59b3091aafb
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-11-01 14:59:36 -0700

    adding changes

--- src/decoder.v
+++ src/decoder.v
@@ -1,38 +1,49 @@
+`default_nettype none
 
-/*
-      -- 1 --
-     |       |
-     6       2
-     |       |
-      -- 7 --
-     |       |
-     5       3
-     |       |
-      -- 4 --
-*/
-
-module seg7 (
-    input wire [3:0] counter,
-    output reg [6:0] segments
+module perceptron (
+    input wire [7:0] current,
+    input wire       clk,
+    input wire       rst_n,
+    output reg [1:0] classification,
 );
 
-    always @(*) begin
-        case(counter)
-            //                7654321
-            0:  segments = 7'b0111111;
-            1:  segments = 7'b0000110;
-            2:  segments = 7'b1011011;
-            3:  segments = 7'b1001111;
-            4:  segments = 7'b1100110;
-            5:  segments = 7'b1101101;
-            6:  segments = 7'b1111100;
-            7:  segments = 7'b0000111;
-            8:  segments = 7'b1111111;
-            9:  segments = 7'b1100111;
-            default:    
-                segments = 7'b0000000;
-        endcase
+    reg [7:0] next_state, threshold
+    reg [7:0] bias
+    reg reg [7:0] weights [0:6]  // Declare an array of 7 registers, each with 8 bits
+
+    always @(posedge clk) begin
+        if (rst_n) begin
+            // Reset the counter and output bit
+            bit_counter <= 3'b0;
+            bit_out <= 1'b0;
+            sum = 0
+
+            // Initialize the weights if needed
+            weights[0] = 8'b10000000;   // The representation here is with the decimal at the from .000000000 
+            weights[1] = 8'b10000000;   // so b1000000 represents 0.5 in decimal
+            weights[2] = 8'b10000000;
+            weights[3] = 8'b10000000;
+            weights[4] = 8'b10000000;
+            weights[5] = 8'b10000000;
+            weights[6] = 8'b10000000;
+        end else begin
+            // Increment the counter
+            bit_counter <= bit_counter + 1'b1;
+            
+            // Output the current bit and update the output
+            bit_out <= current[bit_counter];
+            if (bit_out == 1) begin
+                sum += weight[bit_counter];
+            end else begin
+                ...
+            end
+        end
     end
+
+    // next state logic
+    next_state = curren * weight + bias
+
+    assign classification = (state >= 0)
 
 endmodule
 
