c1b8e1e9e47b40db0a708dc2b4ebfc409ac6cb93
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-11-02 12:00:44 -0700

    edited some stuff

--- src/decoder.v
+++ src/decoder.v
@@ -7,17 +7,19 @@ module perceptron (
     output reg [1:0] classification,
 );
 
-    reg [7:0] next_state, threshold
-    reg [7:0] bias
-    reg reg [7:0] weights [0:6]  // Declare an array of 7 registers, each with 8 bits
+    // reg [7:0] next_state, threshold;
+    reg [7:0] bias, sum;
+    reg reg [7:0] weights [0:6];  // Declare an array of 7 registers, each with 8 bits
+    reg [2:0] bit_counter;
+    reg [0] bit_out;
 
     always @(posedge clk) begin
         if (rst_n) begin
             // Reset the counter and output bit
             bit_counter <= 3'b0;
             bit_out <= 1'b0;
-            sum = 0
-
+            sum = 8'b00000000;
+            bias = 8'b00000000;
             // Initialize the weights if needed
             weights[0] = 8'b10000000;   // The representation here is with the decimal at the from .000000000 
             weights[1] = 8'b10000000;   // so b1000000 represents 0.5 in decimal
@@ -32,18 +34,23 @@ module perceptron (
             
             // Output the current bit and update the output
             bit_out <= current[bit_counter];
+
             if (bit_out == 1) begin
-                sum += weight[bit_counter];
+                sum += weights[bit_counter];
             end else begin
-                ...
+                sum += 0    // The bit out is zero so 0*weight = 0 so the sum doesn't change/gets 0 added to it
+            end
+
+            if (bit_counter == 7) begin
+                if (sum + bias >= 0) begin
+                    classification <= 1;
+                end else begin 
+                    classification <= 0;
+                end 
             end
         end
     end
 
-    // next state logic
-    next_state = curren * weight + bias
-
-    assign classification = (state >= 0)
 
 endmodule
 
