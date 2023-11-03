1ff96a44e455779f4ce6cf682674ce2a081001a4
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-11-02 19:20:00 -0700

    fixed errors where we didn't use our bidirectional

--- src/tt_um_perceptron.v
+++ src/tt_um_perceptron.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_perceptron #( parameter MAX_COUNT = 24'd10_000_000 ) (
+module tt_um_perceptron #() (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -11,8 +11,12 @@ module tt_um_perceptron #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+
     // use bidirectionals as outputs
-    assign uio_oe = 8'b10101010;
+    assign uio_oe = 8'b00000000;
+    
+    // assign the bidirectionals that we are using to 0
+    assign uio_out = 8'b00000000;
 
     // assigning the uo out unused bits to something
     assign uo_out[6:0] = 7'b0000000;
