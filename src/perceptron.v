ca75ec9ac343a5fed990275f1f2b392ca39898dd
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-11-02 19:00:05 -0700

    working perceptron

--- src/perceptron.v
+++ src/perceptron.v
@@ -8,26 +8,28 @@ module perceptron (
 );
 
     // reg [7:0] next_state, threshold;
-    reg [7:0] bias, sum;
+    reg [7:0] bias;
+    reg [8:0] sum;
     reg [7:0] weights [0:6];  // Declare an array of 7 registers, each with 8 bits
     reg [2:0] bit_counter;
-    reg [0:0] bit_out;
+    reg [0:0] bit_out, overflow;
 
     always @(posedge clk) begin
         if (!rst_n) begin
             // Reset the counter and output bit
             bit_counter <= 3'b000;
             bit_out <= 1'b0;
-            sum = 8'b00000000;
-            bias = 8'b00000000;
+            sum <= 9'b000000000;
+            bias <= 8'b00000000;
             // Initialize the weights if needed
-            weights[0] = 8'b10000000;   // The representation here is with the decimal at the from .000000000 
-            weights[1] = 8'b10000000;   // so b1000000 represents 0.5 in decimal
-            weights[2] = 8'b10000000;
-            weights[3] = 8'b10000000;
-            weights[4] = 8'b10000000;
-            weights[5] = 8'b10000000;
-            weights[6] = 8'b10000000;
+            weights[0] <= 8'b10000000;   // The representation here is with the decimal at the from .000000000 
+            weights[1] <= 8'b10000000;   // so b1000000 represents 0.5 in decimal
+            weights[2] <= 8'b10000000;
+            weights[3] <= 8'b10000000;
+            weights[4] <= 8'b10000000;
+            weights[5] <= 8'b10000000;
+            weights[6] <= 8'b10000000;
+            // classification <= 0
         end else begin
             // Increment the counter
             bit_counter <= bit_counter + 1'b1;
@@ -35,14 +37,22 @@ module perceptron (
             // Output the current bit and update the output
             bit_out <= current[bit_counter];
 
+            // set the overflow check bit to 0
+            overflow = 0;
+
             if (bit_out == 1) begin
                 sum += weights[bit_counter];
+
+                if (sum[8] == 1) begin
+                    sum <= 9'b011111111;
+                end
+
             end else begin
                 sum += 0;    // The bit out is zero so 0*weight = 0 so the sum doesn't change/gets 0 added to it
             end
 
             if (bit_counter == 7) begin
-                if (sum + bias >= 8'b1000000) begin
+                if (sum[7:0] + bias >= 8'b1000000) begin
                     classification <= 1;
                 end else begin 
                     classification <= 0;
--- src/test.py
+++ src/test.py
@@ -10,7 +10,7 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_my_design1(dut):  #dut = device under test
 
-    CONSTANT_INPUT = my_variable = 0b10101010 # we can do up to 2^8-1 which is 255
+    CONSTANT_INPUT = my_variable = 0b10101010 # This represents 4 inputs of 1 and 0 alternating
 
     dut._log.info("starting simulation...")
 
@@ -34,7 +34,55 @@ async def test_my_design1(dut):  #dut = device under test
 @cocotb.test()
 async def test_my_design2(dut):  #dut = device under test
 
-    CONSTANT_INPUT = my_variable = 0b00000001 # we can do up to 2^8-1 which is 255
+    CONSTANT_INPUT = my_variable = 0b00000000 # This represents 7 inputs of 0  and 1 input of 1
+
+    dut._log.info("starting simulation...")
+
+    # initializing clock
+    clock = Clock(dut.clk, 1, units="ns")
+    cocotb.start_soon(clock.start())
+
+    dut.rst_n.value = 0 # active low reset
+
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1 # take out of reset
+
+    dut.ui_in.value = CONSTANT_INPUT
+    dut.ena.value = 1 # enable design
+
+    for _ in range(100):
+        await RisingEdge(dut.clk)
+
+    dut._log.info("done?")
+
+@cocotb.test()
+async def test_my_design3(dut):  #dut = device under test
+
+    CONSTANT_INPUT = my_variable = 0b00000001 # This represents 7 inputs of 0  and 1 input of 1
+
+    dut._log.info("starting simulation...")
+
+    # initializing clock
+    clock = Clock(dut.clk, 1, units="ns")
+    cocotb.start_soon(clock.start())
+
+    dut.rst_n.value = 0 # active low reset
+
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1 # take out of reset
+
+    dut.ui_in.value = CONSTANT_INPUT
+    dut.ena.value = 1 # enable design
+
+    for _ in range(100):
+        await RisingEdge(dut.clk)
+
+    dut._log.info("done?")
+
+@cocotb.test()
+async def test_my_design4(dut):  #dut = device under test
+
+    CONSTANT_INPUT = my_variable = 0b00000011 # This represents 6 inputs of 0  and 5 input of 1
 
     dut._log.info("starting simulation...")
 
