166f0f6238e15e3f5bac62cb67b38a0841f8e997
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-11-03 23:31:09 -0700

    continue to udate parent file and related vars

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_perceptron tt_um_perceptron(
+    tt_um_perceptron_neuromeme tt_um_perceptron_neuromeme(
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
